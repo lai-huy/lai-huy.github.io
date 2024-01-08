61781df44947e5a7a5c11e9d2a42c1d9919be426
Huy Lai <lai.huy@tamu.edu>
2024-01-08 12:47:54 -0600

    layout fix

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -4,15 +4,14 @@ import "./index.scss";
 
 const Layout = () => {
 	return (
-		<>
-			<div className="App">
-				<Navigation />
-				<div className="page">
-					<Outlet />
-				</div>
+        <div className="App">
+		    <Navigation />
+			<div className="page">
+				<Outlet />
 			</div>
-		</>
+		</div>
 	);
 };
 
 export default Layout;
+
