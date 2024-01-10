7fcadfe89e27cd79a1efbf2f6dbaaa43acf52dfb
Huy Lai <lai.huy@tamu.edu>
2024-01-10 12:38:54 -0600

    add pos to portfolio

--- src/components/Portfolio/index.tsx
+++ src/components/Portfolio/index.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+
 import { CucumberPlain, RspecOriginal } from "devicons-react";
 import "./portfolio.scss";
 
@@ -69,7 +70,7 @@ const Portfolio = () => {
                     that a team of students and I worked on.
                 </p>
             </div>
-            
+
             <p className="youtube">
                 <iframe
                     className="youtube"
@@ -79,7 +80,7 @@ const Portfolio = () => {
                     allowFullScreen
                 />
             </p>
-            
+
             <a href="#robomasters" className="skip-link">
                 Skip to TAMU RoboMasters
             </a>
@@ -139,6 +140,42 @@ const Portfolio = () => {
                     allowFullScreen
                 />
             </p>
+
+            <a href="#pos" className="skip-link">
+                Skip to point of sale system
+            </a>
+            <div className="h2" role="heading" aria-level={2}>
+                Point of Sale System
+            </div>
+            <div id="pos">
+                <img
+                    className="left_wrap"
+                    src={process.env.PUBLIC_URL + "/assets/images/CFA_MSC.jpg"}
+                    alt="Chick-fil-A at the Memorial Student Center"
+                />
+                <p className="pos">
+                    A Node.js and React project that creates a Point-of-Sale
+                    webpage for a Restaurant located at the Memorial Student
+                    Center at Texas A&M University. Our specific restaurant 
+                    was Chick-fil-A.
+                    <br />
+                    Using Google OAuth to authenticate administrator logins,
+                    adequate role-based access control is enforced.
+                    <br />
+                    This project also uses a Google Translate API to ensure
+                    proper internationalization.
+                    <br />
+                    Finally, this project uses a weather API to display weather
+                    information as well as recomend certain items during cooler
+                    weather.
+                    <br />
+                    A custom REST API to communicate between the front-end and
+                    the back-end of the application.
+                    <br />
+                    <br />
+                    You can view the source code for this project <a href="https://github.com/CSCE315-Spring23/Team_44_Project_3">here</a>.
+                </p>
+            </div>
 
             <a href="#chess" className="skip-link">
                 Skip to chess
@@ -250,4 +287,3 @@ const Portfolio = () => {
 };
 
 export default Portfolio;
-
