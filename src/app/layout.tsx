5d9c822e0a5e73b87fc91844b7bfab2c76c20463
Huy Lai <98334390+lai-huy@users.noreply.github.com>
2025-01-09 21:33:15 -0600

    metadata stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,25 @@ import { Navbar } from "./navigation/nav";
 export const metadata: Metadata = {
 	title: "Huy Quang Lai",
 	description: "My personal website",
+	openGraph: {
+		title: "Huy Quang Lai",
+		description: "My personal website",
+		images: [
+			{
+				url: "/assets/images/face.jpg",
+				alt: "Huy Lai",
+				width: 1851,
+				height: 2600,
+			},
+		],
+		type: "website",
+	},
+	twitter: {
+		title: "Huy Quang Lai",
+		description: "My personal website",
+		card: "summary_large_image",
+		images: ["/assets/images/face.jpg"],
+	},
 };
 
 export default function RootLayout({
