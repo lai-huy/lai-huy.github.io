d0b893635eb48713979c2544df41e0a00bc26c8e
Huy Lai <98334390+lai-huy@users.noreply.github.com>
2025-02-18 16:18:03 -0600

    more pdf frames

--- src/app/qualifications/awards.tsx
+++ src/app/qualifications/awards.tsx
@@ -1,3 +1,8 @@
+const bs_cs: string = String.raw`https://raw.githubusercontent.com/lai-huy/lai-huy.github.io/main/public/assets/awards/bs_cs.pdf`;
+const fall22: string = String.raw`https://raw.githubusercontent.com/lai-huy/lai-huy.github.io/main/public/assets/awards/fall22.pdf`;
+const spring23: string = String.raw`https://raw.githubusercontent.com/lai-huy/lai-huy.github.io/main/public/assets/awards/spring23.pdf`;
+const AP_scholar: string = String.raw`https://raw.githubusercontent.com/lai-huy/lai-huy.github.io/main/public/assets/awards/ap_scholar.pdf`;
+
 export default function Awards() {
 	return (
 		<div id="awards">
@@ -8,22 +13,22 @@ export default function Awards() {
 				<iframe
 					className="award"
 					title="Bachelor of Science in Computer Science"
-					src="/assets/awards/bs_cs.pdf"
+					src={`https://mozilla.github.io/pdf.js/web/viewer.html?file=${bs_cs}#zoom=page-width&sidebar=0`}
 				/>
 				<iframe
 					className="award"
 					title="Distinguished Student Award"
-					src="/assets/awards/fall22.pdf"
+					src={`https://mozilla.github.io/pdf.js/web/viewer.html?file=${fall22}#zoom=page-width&sidebar=0`}
 				/>
 				<iframe
 					className="award"
 					title="Dean's Honor Roll"
-					src="/assets/awards/spring23.pdf"
+					src={`https://mozilla.github.io/pdf.js/web/viewer.html?file=${spring23}#zoom=page-width&sidebar=0`}
 				/>
 				<iframe
 					className="award"
 					title="AP Scholar Award"
-					src="/assets/awards/ap_scholar.pdf"
+					src={`https://mozilla.github.io/pdf.js/web/viewer.html?file=${AP_scholar}#zoom=page-width&sidebar=0`}
 				/>
 			</p>
 		</div>
