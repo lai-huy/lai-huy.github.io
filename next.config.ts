58c17881d0e093a10c4b74d01d13ff614982b4c9
Huy Lai <98334390+lai-huy@users.noreply.github.com>
2025-03-09 16:07:34 -0500

    config

--- next.config.ts
+++ next.config.ts
@@ -3,17 +3,29 @@ import type { NextConfig } from "next";
 let envImageUnoptimize = process.env.NODE_ENV !== "production" ? false : true;
 
 const nextConfig: NextConfig = {
-    output: process.env.NODE_ENV !== "production" ? undefined: "export",
-    trailingSlash: true,
-    reactStrictMode: true,
-    images: {
-        unoptimized: envImageUnoptimize,
-        remotePatterns: [
-            {
-                "hostname": "images.unsplash.com",
-            }
-        ]
-    },
+	output: process.env.NODE_ENV !== "production" ? undefined : "export",
+	trailingSlash: true,
+	reactStrictMode: true,
+	images: {
+		unoptimized: envImageUnoptimize,
+		remotePatterns: [
+			{
+				hostname: "images.unsplash.com",
+			},
+			{
+				hostname: "raw.githubusercontent.com",
+			},
+			{
+				hostname: "app.fide.com",
+			},
+			{
+				hostname: "xgboost.ai",
+			},
+			{
+				hostname: "script-search.github.io",
+			},
+		],
+	},
 };
 
 export default nextConfig;
