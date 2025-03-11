3145d41a65654edb3a734e7c403cac8923aae7ff
Huy Lai <98334390+lai-huy@users.noreply.github.com>
2025-03-11 17:25:46 -0500

    change home screen

--- src/app/home/about.tsx
+++ src/app/home/about.tsx
@@ -3,6 +3,7 @@ const bs_cs: string = String.raw`https://catalog.tamu.edu/undergraduate/engineer
 const cyber_minor: string = String.raw`https://catalog.tamu.edu/undergraduate/engineering/cybersecurity-minor/#programrequirementstext`;
 const math_minor: string = String.raw`https://catalog.tamu.edu/undergraduate/arts-and-sciences/mathematics/minor/#programrequirementstext`;
 const tamu: string = String.raw`https://www.tamu.edu/`;
+
 export default function About() {
 	return (
 		<div>
@@ -16,37 +17,16 @@ export default function About() {
 				<p className="about">
 					Howdy! I am a student at{" "}
 					<a href={tamu}>Texas A&M University</a> currently studying
-					for a Master of Computer Science.
-				</p>
-			</div>
-			<a href="#prog" className="skip-link">
-				Skip to MCS Requirements
-			</a>
-			<div id="prog">
-				<p className="prog_req">
-					Learn more about the <a href={mcs}>MCS</a>.
-				</p>
-			</div>
-			<div id="about">
-				<p className="about">
-					I received my Bachelor of Science in Computer Science from{" "}
-					<a href={tamu}>Texas A&M University</a> .
-					<br />I also have a Minor in Cybersecurity and a Minor in
-					Mathematics.
-				</p>
-			</div>
-
-			<a href="#prog" className="skip-link">
-				Skip to BS Requirements
-			</a>
-			<div id="prog">
-				<p className="prog_req">
-					Learn more about the{" "}
-					<a href={bs_cs}>BS in Computer Science</a>.<br />
-					Learn more about the{" "}
-					<a href={cyber_minor}>Minor in Cybersecurity</a>.<br />
-					Learn more about the{" "}
-					<a href={math_minor}>Minor in Mathematics</a>.
+					for a <a href={mcs}>Master of Computer Science</a>.
+					<br />
+					<br />I received my{" "}
+					<a href={bs_cs}>
+						Bachelor of Science Computer Science
+					</a>{" "}
+					from <a href={tamu}>Texas A&M University</a> .
+					<br />I also have a{" "}
+					<a href={cyber_minor}>Minor in Cybersecurity</a> and a{" "}
+					<a href={math_minor}> Minor in Mathematics</a>.
 				</p>
 			</div>
 		</div>
